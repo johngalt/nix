220040fe01fbb329a33b8222e6e3b2ce09aadf20
johngalt <322757+johngalt@users.noreply.github.com>
2025-12-17 13:51:05 -0600

    moved dms theming to separate file to clean things up

--- modules/desktop/niri/dms/default.nix
+++ modules/desktop/niri/dms/default.nix
@@ -14,7 +14,7 @@ let
 in 
 {
   imports = [
-    ./matugen
+    ./theming.nix
   ];
 
   config = mkIf (cfg.shell == "dms" && cfg.enable) {
@@ -50,23 +50,6 @@ in
 
     # Additional system packages to install with DMS
     environment.systemPackages = with pkgs; [
-      # KDE/QT Package dependencies for DMS
-      # NixOS and QT doesn't play well on non-Plasma setups
-      # Ideally I would use the qt6ct-kde patched package from AUR if I was on Arch
-      libsForQt5.qt5ct
-      kdePackages.qt6ct
-      kdePackages.kcolorscheme
-      kdePackages.breeze
-      kdePackages.breeze.qt5
-
-      # Theme stuff
-      # For KDE Apps (dolphin) need to change Configure -> Window Color Scheme
-      adw-gtk3 # Support dynamic theming for gtk3 applications
-      adwaita-qt
-      adwaita-qt6
-      papirus-icon-theme # Icon theme
-      papirus-folders
-
       # Applications
       swappy # Screenshot annotation
       seahorse # Gnome secrets manager
@@ -86,10 +69,8 @@ in
       pkgs.nautilus
     ];
 
-    # Environment variables for DMS and QT theming things
+    # Environment variables for DMS/Wayland
     environment.variables = {
-      QT_QPA_PLATFORMTHEME = "qt6ct";
-      QT_QPA_PLATFORMTHEME_QT6 = "qt6ct";
       QT_QPA_PLATFORM = "wayland";
       ELECTRON_OZONE_PLATFORM_HINT = "auto";
     };
@@ -158,26 +139,6 @@ in
           include "dms/wpblur.kdl"
         '';
       };
-      # Force QT/KDE applications like Dolphin to use qt6ct and correct icon themes
-      # Some QT/KDE apps just ignore the ~/.config/qt{5,6}ct/ folders
-      files.".config/kdeglobals".text = ''
-        [UiSettings]
-        ColorScheme=qt6ct
-
-        [Icons]
-        Theme=Papirus-Dark
-      '';
     };
-    # gsettings uses dconf to store settings -- set default icon pack
-    # i'm sure there is a way to get ~/.config/gtk-{3,4}.0/settings.ini to work but its not on Nix
-    programs.dconf.profiles.user.databases = [
-      {
-        settings = {
-          "org/gnome/desktop/interface" = {
-            icon-theme = "Papirus-Dark";
-          };
-        };
-      }
-    ];
   };
 }
---
+++ modules/desktop/niri/dms/theming.nix
@@ -0,0 +1,68 @@
+{
+  config,
+  pkgs,
+  lib,
+  ...
+}:
+let 
+  cfg = config.custom.desktop.niri;
+
+  inherit (lib)
+    mkIf
+    ;
+in 
+{
+  imports = [
+    ./matugen
+  ];
+
+  config = mkIf (cfg.enable && cfg.shell == "dms") {
+    # Set QT apps to follow qt6ct theme settings by default    
+    environment.variables = {
+      QT_QPA_PLATFORMTHEME = "qt6ct";
+      QT_QPA_PLATFORMTHEME_QT6 = "qt6ct";
+    };
+
+    environment.systemPackages = with pkgs; [
+      # KDE/QT Package dependencies for DMS
+      # NixOS and QT doesn't play well on non-Plasma setups
+      # Ideally I would use the qt6ct-kde patched package from AUR if I was on Arch
+      libsForQt5.qt5ct
+      kdePackages.qt6ct
+      kdePackages.kcolorscheme
+      kdePackages.breeze
+      kdePackages.breeze.qt5
+
+      # Theme stuff
+      # For KDE Apps (dolphin) need to change Configure -> Window Color Scheme
+      adw-gtk3 # Support dynamic theming for gtk3 applications
+      papirus-icon-theme # Icon theme
+      catppuccin-papirus-folders.override { # Override to specify folder colors
+        flavor = "macchiato";
+        color = "blue";
+      }
+    ];
+
+    # Force QT/KDE applications like Dolphin to use qt6ct and correct icon themes
+    # Some QT/KDE apps just ignore the ~/.config/qt{5,6}ct/ folders
+    custom.hjem.cfg.files.".config/kdeglobals".text = ''
+      [UiSettings]
+      ColorScheme=qt6ct
+
+      [Icons]
+      Theme=Papirus-Dark
+    '';
+
+    # gsettings uses dconf to store settings -- set default icon pack
+    # i'm sure there is a way to get ~/.config/gtk-{3,4}.0/settings.ini to work but its not on Nix
+    programs.dconf.profiles.user.databases = [
+      {
+        settings = {
+          "org/gnome/desktop/interface" = {
+            icon-theme = "Papirus-Dark";
+          };
+        };
+      }
+    ];
+  };
+}
