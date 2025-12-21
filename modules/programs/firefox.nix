0d6243d90d5f9a397a046915ab0c8f9aad1b202a
johngalt <322757+johngalt@users.noreply.github.com>
2025-12-21 15:25:32 -0600

    adding policies to firefox module

--- modules/programs/firefox.nix
+++ modules/programs/firefox.nix
@@ -19,7 +19,56 @@ in
   config = mkIf cfg.enable {
     programs.firefox = {
       enable = true;
+      # Custom policies to disable things in firefox declaratively (like AI stuff)
+      policies = {
+        AutofillCreditCardEnabled = false;
+        DisableFirefoxStudies = true;
+        DisableTelemetry = true;
+        DontCheckDefaultBrowser = true;
+        OfferToSaveLogins = false;
+        PasswordManagerEnabled = false;
+        PrimaryPassword = false;
+        SkipTermsOfUse = true;
+        VisualSearchEnabled = false;
+        EnableTrackingProtection = {
+          Cryptomining = true;
+          Fingerprinting = true;
+          EmailTracking = true;
+        };
+
+        FirefoxHome = {
+          Search = false;
+          TopSites = false;
+          SponsoredTopSites = false;
+          Highlights = false;
+          Pocket = false;
+          Stories = false;
+          SponsoredPocket = false;
+          SponsoredStories = false;
+          Snippets = false;
+          Locked = true;
+        };
+
+        FirefoxSuggest = {
+          WebSuggestions = false;
+          SponsoredSuggestions = false;
+          ImproveSuggest = false;
+          Locked = true;
+        };
+
+        GenerativeAI = {
+          Enabled = false;
+          Chatbot = false;
+          LinkPreviews = false;
+          TabGroups = false;
+          Locked = true;
+        };
+
+        Homepage = {
+          StartPage = "none";
+          Locked = true;
+        };
+      };
     };
-    # stylix.targets.firefox.enable = false;
   };
 }
