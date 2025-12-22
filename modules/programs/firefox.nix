7fe52f2f4c15dfc9c790a72bfefd36f7bc4ce386
johngalt <322757+johngalt@users.noreply.github.com>
2025-12-22 15:18:21 -0600

    added more policy settings to firefox

--- modules/programs/firefox.nix
+++ modules/programs/firefox.nix
@@ -19,17 +19,23 @@ in
   config = mkIf cfg.enable {
     programs.firefox = {
       enable = true;
-      # Custom policies to disable things in firefox declaratively (like AI stuff)
+      # Custom policies to disable things in firefox declaratively
       policies = {
         AutofillCreditCardEnabled = false;
+        AutofillAddressEnabled = false;
         DisableFirefoxStudies = true;
+        DisableFeedbackCommands = true;
         DisableTelemetry = true;
+        DisableMasterPasswordCreation = true;
+        DisplayBookmarksToolbar = "always";
         DontCheckDefaultBrowser = true;
         OfferToSaveLogins = false;
         PasswordManagerEnabled = false;
         PrimaryPassword = false;
         SkipTermsOfUse = true;
         VisualSearchEnabled = false;
+        DisablePocket = true;
+        WindowsSSO = false;
         EnableTrackingProtection = {
           Cryptomining = true;
           Fingerprinting = true;
@@ -67,6 +73,15 @@ in
         Homepage = {
           StartPage = "none";
           Locked = true;
+        };
+        UserMessaging = {
+          WhatsNew = false;
+          ExtensionRecommendations = false;
+          FeatureRecommendations = false;
+          UrlbarInterventions = false;
+          SkipOnBoarding = false;
+          MoreFromMozilla = false;
+          FirefoxLabs = false;
         };
       };
     };
