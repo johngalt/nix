2f98c6426efe2d3854f65b0244c6683becdfd1f5
johngalt <322757+johngalt@users.noreply.github.com>
2025-12-25 21:39:35 -0600

    add nixd and nixf overlay to revert version due to regression

--- overlays/default.nix
+++ overlays/default.nix
@@ -14,6 +14,23 @@
       # qt6Packages = prev.qt6Packages.overrideScope (qfinal: qprev: {
       #   qt6ct = prev.qt6Packages.callPackage ../pkgs/qt6ct-kde { };
       # });
+
+      nixd = prev.nixd.overrideAttrs (old: {
+        src = prev.fetchFromGitHub {
+          owner = "nix-community";
+          repo = "nixd";
+          tag = "2.7.0";
+          hash = "sha256-VPUX/68ysFUr1S8JW9I1rU5UcRoyZiCjL+9u2owrs6w=";
+        };
+      });
+      nixf = prev.nixf.overrideAttrs (old: {
+        src = prev.fetchFromGitHub {
+          owner = "nix-community";
+          repo = "nixd";
+          tag = "2.7.0";
+          hash = "sha256-VPUX/68ysFUr1S8JW9I1rU5UcRoyZiCjL+9u2owrs6w=";
+        };
+      });
     })
   ];
 }
