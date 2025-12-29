9985b40ad374194b2ce2767bfc144cacdb7265b0
johngalt <322757+johngalt@users.noreply.github.com>
2025-12-28 20:27:12 -0600

    overlays: cleanup, removing temp fix

--- overlays/default.nix
+++ overlays/default.nix
@@ -10,24 +10,6 @@
       davocache = prev.callPackage ../pkgs/davocache { };
       eza-themes = prev.callPackage ../pkgs/eza-themes { };
       qt6ct-kde = prev.qt6Packages.callPackage ../pkgs/qt6ct-kde { };
-
-      # Fix for bug until 2.8.1 is released in nixpkgs
-      nixd = prev.nixd.overrideAttrs (old: {
-        src = prev.fetchFromGitHub {
-          owner = "nix-community";
-          repo = "nixd";
-          tag = "2.7.0";
-          hash = "sha256-VPUX/68ysFUr1S8JW9I1rU5UcRoyZiCjL+9u2owrs6w=";
-        };
-      });
-      nixf = prev.nixf.overrideAttrs (old: {
-        src = prev.fetchFromGitHub {
-          owner = "nix-community";
-          repo = "nixd";
-          tag = "2.7.0";
-          hash = "sha256-VPUX/68ysFUr1S8JW9I1rU5UcRoyZiCjL+9u2owrs6w=";
-        };
-      });
     })
   ];
 }
