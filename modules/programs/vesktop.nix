26137b969192d527004ad535fcf59c3b86dfdc0b
johngalt <322757+johngalt@users.noreply.github.com>
2026-01-05 09:56:04 -0600

    switching to equibop for discord

--- modules/programs/vesktop.nix
+++ modules/programs/vesktop.nix
@@ -14,30 +14,30 @@ let
 in
 {
   options.custom.programs.discord = {
-    enable = mkEnableOption "Enable Discord (Vesktop)";
+    enable = mkEnableOption "Enable Discord (Equibop)";
   };
 
   config = mkIf cfg.enable {
     custom.hjem.cfg = {
-      packages = with pkgs; [ vesktop ];
-      files.".config/vesktop/settings.json" = {
-        generator = lib.generators.toJSON { };
-        value = {
-          appBadge = false;
-          arRPC = true;
-          checkUpdates = false;
-          customTitleBar = true;
-          disableMinSize = false;
-          minimizeToTray = true;
-          clickTrayToShowHide = true;
-          tray = true;
-          enableSplashScreen = true;
-          splashTheming = true;
-          staticTitle = false;
-          hardwareAcceleration = true;
-          discordBranch = "stable";
-        };
-      };
+      packages = with pkgs; [ equibop ];
+      # files.".config/equibop/settings.json" = {
+      #   generator = lib.generators.toJSON { };
+      #   value = {
+      #     appBadge = false;
+      #     arRPC = true;
+      #     checkUpdates = false;
+      #     customTitleBar = true;
+      #     disableMinSize = false;
+      #     minimizeToTray = true;
+      #     clickTrayToShowHide = true;
+      #     tray = true;
+      #     enableSplashScreen = true;
+      #     splashTheming = true;
+      #     # staticTitle = false;
+      #     hardwareAcceleration = true;
+      #     discordBranch = "stable";
+      #   };
+      # };
     };
   };
 }
