e436a30e675fc095c9129a1d10372e7e4bc50490
johngalt <322757+johngalt@users.noreply.github.com>
2026-02-17 21:51:48 -0600

    system: removing auto-cpufreq module

--- modules/system/auto-cpufreq.nix
+++ modules/system/auto-cpufreq.nix
@@ -1,3 +1,4 @@
+# NOT USED ANYMORE
 {
   config,
   inputs,
--- modules/system/default.nix
+++ modules/system/default.nix
@@ -3,7 +3,7 @@
 }:
 {
   imports = [
-    ./auto-cpufreq.nix
+    # ./auto-cpufreq.nix
     ./docker.nix
     ./impermanence.nix
     ./hjem.nix
