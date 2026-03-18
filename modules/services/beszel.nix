7f5c447294b31fae486c3f8f5a2fbb2284fa76bb
johngalt <322757+johngalt@users.noreply.github.com>
2026-03-18 13:12:31 -0500

    beszel: updating to new module options

--- modules/services/beszel.nix
+++ modules/services/beszel.nix
@@ -21,11 +21,6 @@ in
 {
   options.custom.services.beszel = {
     enable = mkEnableOption "Enable beszel agent";
-    # user = mkOption {
-    #   type = str;
-    #   description = "User to run beszel-hub";
-    #   default = "beszel-agent";
-    # };
     environmentFile = mkOption {
       type = lib.types.path;
       description = "Path to environment file used with beszel-agent service";
@@ -38,32 +33,12 @@ in
   };
 
   config = mkIf cfg.enable {
-    ####
-    # Remove once PR 461327 is merged
-    # https://github.com/NixOS/nixpkgs/pull/461327
-    # users.users.${cfg.user} = {
-    #   isSystemUser = true;
-    #   group = cfg.user;
-    # };
-    # systemd.services.beszel-agent = {
-    #   serviceConfig = {
-    #     SupplementaryGroups = lib.optionals config.virtualisation.docker.enable [ "docker" ] ++ [
-    #       "messagebus"
-    #     ];
-
-    #     DynamicUser = mkForce false;
-    #     Group = "beszel-agent";
-    #   };
-    # };
-    # users.groups.beszel-agent = { };
-    ####
-
     services.beszel.agent = {
       enable = true;
       openFirewall = true;
+      smartmon.enable = true;
       environmentFile = cfg.environmentFile;
       environment = cfg.environment;
-      extraPath = pkgs.intel-gpu-tools;
     };
   };
 }
