03508be920df098095b39543fb4ed375f9f99550
johngalt <322757+johngalt@users.noreply.github.com>
2026-05-09 20:01:05 -0500

    docker: adding `default-address-pools` to keep docker network subnets in check

--- modules/system/docker.nix
+++ modules/system/docker.nix
@@ -21,14 +21,31 @@
       };
 
       config = {
-        virtualisation.docker.enable = true;
-        virtualisation.docker.autoPrune.enable = true;
+        virtualisation.docker = {
+          enable = true;
+          autoPrune.enable = true;
+          daemon.settings = {
+            # Forces docker to keep bridge networks in the same subnet
+            # https://github.com/moby/moby/issues/37823
+            default-address-pools = [
+              {
+                base = "172.17.0.0/16";
+                size = 24;
+              }
+            ];
+          };
+        };
 
         # Create a docker user
         users.users.${dockerUser} = {
           isSystemUser = true;
+          uid = 995; # makes it easier for setting permissions in docker-compose
           group = dockerGroup;
           extraGroups = cfg.extraGroups;
+        };
+
+        users.groups.${dockerGroup} = {
+          gid = 131; # makes it easier for setting permissions in docker-compose
         };
       };
     };
