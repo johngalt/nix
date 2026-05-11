a9fa127b7086bd644b46b9d2a5e311c716cf8fdf
johngalt <322757+johngalt@users.noreply.github.com>
2026-05-10 20:44:58 -0500

    technitium: manually adding new version package

--- modules/services/technitium.nix
+++ modules/services/technitium.nix
@@ -1,8 +1,23 @@
 { ... }:
 {
-  flake.modules.nixos.technitium = {
+  flake.modules.nixos.technitium =
+    { lib, pkgs, ... }:
+    let
+      # TODO: remove when 15.2 gets to nixpkgs
+      technitium-dns-server-library = pkgs.callPackage ../../packages/technitium-dns-server-library/package.nix {};
+      technitium-dns-server = pkgs.callPackage ../../packages/technitium-dns-server/package.nix { inherit technitium-dns-server-library; };
+    in
+    {
+
+    # Networkd enables resoled, need to force it disabled
+    # Resolved tries to bind port 53 which interferes with technitium
+    services.resolved.enable = lib.mkForce false;
+    # Force resolvconf to use local DNS (technitium)
+    networking.resolvconf.useLocalResolver = lib.mkForce true;
+
     services.technitium-dns-server = {
       enable = true;
+      package = technitium-dns-server;
       openFirewall = true;
       firewallUDPPorts = [
         53
