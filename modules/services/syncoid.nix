c3982987f91d90e5a5f9258269d23b51e2ebc345
johngalt <322757+johngalt@users.noreply.github.com>
2026-05-26 21:24:02 -0500

    syncoid: tweaking module for use on my laptop as well

--- modules/services/syncoid.nix
+++ modules/services/syncoid.nix
@@ -11,16 +11,23 @@
       generateCommands = lib.genAttrs cfg.datasets (set: {
         source = set;
         target = "${cfg.targetHost}:${cfg.targetRoot}/${trimDataset set}";
-        # Only add extra systemd service options for FIRST dataset to avoid duplicating healthchecks
-        service = lib.mkIf (!isNull cfg.healthcheckId) (lib.optionalAttrs (set == (lib.head cfg.datasets)) {
-          onFailure = [ "ping-healthchecks@${cfg.healthcheckId}:failure.service" ];
-          onSuccess = [ "ping-healthchecks@${cfg.healthcheckId}:success.service" ];
-          wants = [ "ping-healthchecks@${cfg.healthcheckId}:start.service" ];
-        });
+        # This is really ugly logic...
+        service = lib.mkMerge [
+          # Only add service config to ping healthcheck if healthcheckId has been defined
+          (lib.mkIf (!isNull cfg.healthcheckId)
+            # Only add extra systemd service options for FIRST dataset to avoid duplicating healthchecks
+            (lib.optionalAttrs (set == (lib.head cfg.datasets)) {
+              onFailure = [ "ping-healthchecks@${cfg.healthcheckId}:failure.service" ];
+              onSuccess = [ "ping-healthchecks@${cfg.healthcheckId}:success.service" ];
+              wants = [ "ping-healthchecks@${cfg.healthcheckId}:start.service" ];
+            }))
+          # Merge in attributes defined in extraServiceConfig
+          { serviceConfig = cfg.serviceConfig; }
+        ];
       });
 
       inherit (lib) mkOption;
-      inherit (lib.types) listOf str nullOr anything;
+      inherit (lib.types) listOf str nullOr anything attrs;
 
     in
     {
@@ -48,6 +55,11 @@
           description = "OnCalendar timer to run. Set as empty list to disable automatic running";
           default = "*-*-* 00/2:00:00";
         };
+        serviceConfig = mkOption {
+          type = attrs;
+          description = "Extra service config attributes to pass to systemd service files";
+          default = { };
+        };
       };
 
       config = {
@@ -57,22 +69,31 @@
           zstd # Compression used by zfs send/receive
         ];
 
+        # Pull private ssh key from sops
         sops.secrets."syncoid/sshKey" = {
           owner = "syncoid";
           group = "syncoid";
           mode = "0700";
         };
 
+        # Adding public key for target host verification
+        services.openssh.knownHosts = {
+          "argon" = {
+            hostNames = [ "192.168.10.10" "argon.gudhak.home" ];
+            publicKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAiYEfYEsnr25d2wL9yhMbecWF13/sDCT7fiKASPDKIQ";
+          };
+        };
+
         services.syncoid = {
           enable = true;
           interval = cfg.interval;
+          # Use the syncoid user defined on backup target to handle the zfs send/rcv
           user = "syncoid";
           sshKey = config.sops.secrets."syncoid/sshKey".path;
           commonArgs = [
             "--delete-target-snapshots" # Keep snapshots in sync (so they don't pile up on target)
             "--force-delete" # Removes conflicting snapshots on target
-            "--no-privilege-elevation" 
-            "--sshoption=\"StrictHostKeyChecking=no\""
+            "--no-privilege-elevation"
             "--compress=zstd-fast"
           ];
 
