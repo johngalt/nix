10fc999366b98046f0b8182bbb52153d778856f9
johngalt <322757+johngalt@users.noreply.github.com>
2026-06-01 06:43:49 -0500

    dms: cleaning up matugen

--- modules/desktop/dms/matugen.nix
+++ modules/desktop/dms/matugen.nix
@@ -16,17 +16,9 @@
           input_path = '${homeDir}/.config/matugen/themes/btop.theme'
           output_path = '${homeDir}/.config/btop/themes/matugen.theme'
 
-          # [templates.yazi]
-          # input_path = '${homeDir}/.config/matugen/themes/yazi-theme.toml'
-          # output_path = '${homeDir}/.config/yazi/theme.toml'
-
           [templates.vesktop]
           input_path = '${homeDir}/.config/matugen/themes/vesktop.css'
           output_path = '${homeDir}/.config/vesktop/themes/dank-modified.css'
-
-          # [templates.helix]
-          # input_path = '${homeDir}/.config/matugen/themes/helix.toml'
-          # output_path = '${homeDir}/.config/helix/themes/matugen.toml'
 
           [templates.zathura]
           input_path = '${homeDir}/.config/matugen/themes/zathura-colors'
