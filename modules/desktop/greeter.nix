dd5db9746432db9cd3297b219b24249efe607a01
johngalt <322757+johngalt@users.noreply.github.com>
2026-06-01 06:44:05 -0500

    greeter: switching to tuigreet

---
+++ modules/desktop/greeter.nix
@@ -0,0 +1,47 @@
+{ inputs, ... }:
+{
+  flake.modules.nixos.greeter =
+    { config, pkgs, lib, ... }:
+    let
+      # tuigreet from NotAShelf's fork
+      tuigreetPackage = inputs.tuigreet.packages.${pkgs.stdenv.hostPlatform.system}.tuigreet;
+      # use nixpkgs displayManager module to get desktop session files to load from
+      sessionFiles = "${config.services.displayManager.sessionData.desktops}/share/wayland-sessions";
+    in
+    {
+      services.greetd = {
+        enable = true;
+        settings = {
+          default_session = {
+            command = "${lib.getExe tuigreetPackage} --asterisks --remember --time --sessions ${sessionFiles}";
+            user = "greeter";
+          };
+        };
+      };
+
+      # persist preservation cache dir
+      custom.system.preservation.extraCacheDirectories = [ "/var/cache/tuigreet" ];
+
+      # lets make the console prettier
+      console = {
+        colors = [
+          "282828"
+          "ea6962"
+          "a9b665"
+          "d8a657"
+          "7daea3"
+          "d3869b"
+          "89b482"
+          "d4be98"
+          "928374"
+          "ea6962"
+          "a9b665"
+          "d8a657"
+          "7daea3"
+          "d3869b"
+          "89b482"
+          "ddc7a1"
+        ];
+      };
+    };
+}
